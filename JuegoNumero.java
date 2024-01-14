df14ea252fbe9ddee6d132bc5c7123f0c127e911
Malunigs <149101380+Malunigs@users.noreply.github.com>
2024-01-14 20:00:37 +0100

    Add files via upload
    Código terminado.

--- JuegoNumero.java
+++ JuegoNumero.java
@@ -6,6 +6,7 @@ public class JuegoNumero {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int opcion=0;
+
         do{
         System.out.println("Bienvenido al juego de adivinar el numero entre 0 y 100.");
         System.out.println("El programa tiene dos modos de juego, elija la opcion a la que desee jugar:");
@@ -28,8 +29,19 @@ public class JuegoNumero {
                 System.out.println("Opcion no valida.");
                 break;     
         }
-        teclado.close();
+
+        System.out.println("¿Desea volver a jugar? (1: Sí / 0: No)");
+        int jugarNuevamente = teclado.nextInt();
+    
+        if (jugarNuevamente == 1) {
+            main(args);
+        } 
+        else{
+            System.out.println("¡Gracias por jugar! Hasta luego.");
         }
+
+        teclado.close();
+    }
 
 
     public static void primerModoJuego(){    
@@ -50,15 +62,17 @@ public class JuegoNumero {
             if (intento == numeroAdivinar) {
                 acertado = true;
                 System.out.println("¡Felicidades! ¡Has adivinado el número!");
-            } else if (intento < numeroAdivinar) {
+                break;
+            } 
+            else if(intento < numeroAdivinar) {
                 System.out.println("El número a adivinar es mayor. Intenta de nuevo.");
-            } else {
+            } 
+            else{
                 System.out.println("El número a adivinar es menor. Intenta de nuevo.");
             }
         }
-
-        teclado.close();
     }
+
     public static void segundoModoJuego(){    
         Random random = new Random();
         int numeroAdivinar = random.nextInt(101);
@@ -69,42 +83,54 @@ public class JuegoNumero {
         int numIntento=1;
         int intento=-1;
         boolean acertado = false;
-        out:
+
         while (acertado == false) {
             if (intento == numeroAdivinar){
                 System.out.println("¡Felicidades! ¡Has adivinado el número!");
                 acertado = true;
-                break out;
-            }else{
-            System.out.println("Intrdouce tu intento numero "+numIntento);
+                break;
+            }
+            else{
+            System.out.println("Introduce tu intento número "+numIntento);
             }
             switch (numIntento) {
                 case 1:
                     if(numeroAdivinar <= 50 ){
-                        System.out.println("El numero a adivinar esta entre [0,50].");
-                    }else{
-                        System.out.println("El numero a adivinar esta entre [51,100]. ");
+                        System.out.println("El número a adivinar esta entre [0,50].");
+                    }
+                    else{
+                        System.out.println("El número a adivinar esta entre [51,100]. ");
                     }
                     break;
                 case 2:
                     if(numeroAdivinar%2 == 0){
-                        System.out.println("El numero a adivinar es par.");
-                    }else{
-                        System.out.println("El numero a adivinar es impar.");
+                        System.out.println("El número a adivinar es par.");
+                    }
+                    else{
+                        System.out.println("El número a adivinar es impar.");
                     }
                     break;
                 case 3:
-                    System.out.println("El numero a adivinar empieza por "+primerDigito(numeroAdivinar));    
-                    
+                    if(esPrimo(numeroAdivinar)== true){
+                        System.out.println("El número a adivinar es primo.");
+                    }
+                    else{
+                        System.out.println("El número a adivinar no es primo.");
+                    }
+                    break;
+                case 4:
+                    System.out.println("El número a adivinar empieza por "+primerDigito(numeroAdivinar));    
+                    break;
+                case 5:
+                    System.out.println("El número a adivinar se encuentra entre "+(numeroAdivinar-1)+" y "+(numeroAdivinar+1)+".");
                 default:
                     break;
             }
             intento = teclado.nextInt();
             numIntento++;
+        } 
+    }
 
-    }
-    teclado.close();
-    }
     public static int primerDigito(int numero){
         String numeroComoCadena = Integer.toString(numero);
         int primerDigito = Character.getNumericValue(numeroComoCadena.charAt(0));
@@ -112,5 +138,18 @@ public class JuegoNumero {
         return primerDigito;
 
     }
+
+    public static boolean esPrimo(int numero){
+
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                //Si el número es divisible por algún otro número, no es primo
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
 
+    
